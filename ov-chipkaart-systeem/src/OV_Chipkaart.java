2234bc26d258a558d7d4632016b82496bd1b46da
Yonaki51 <yonivandon@hotmail.com>
2026-03-09 14:29:04 +0100

    ov kaart gemaakt

--- ov-chipkaart-systeem/src/OV_Chipkaart.java
+++ ov-chipkaart-systeem/src/OV_Chipkaart.java
@@ -1,7 +1,25 @@
 public class OV_Chipkaart {
     private double saldo;
-    private String OV_nummer;
+    private String chipNr;
     private boolean geldig;
     private boolean isIngecheckt;
 
+    public OV_Chipkaart(double saldo, String chipNr, boolean geldig) {
+        this.saldo = saldo;
+        this.chipNr = chipNr;
+        this.geldig = geldig;
+        this.isIngecheckt = false;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean getGeldig() {
+        return geldig;
+    }
+
+    public void setIsIngecheckt(boolean isIngecheckt) {
+        this.isIngecheckt = isIngecheckt;
+    }
 }
