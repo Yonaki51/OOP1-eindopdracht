5190b03221761d7d5c1bd60fad11ef65c4ce763e
Yonaki51 <yonivandon@hotmail.com>
2026-03-09 14:28:48 +0100

    paal voor inchecken gemaakt

--- ov-chipkaart-systeem/src/Check_In_Paal.java
+++ ov-chipkaart-systeem/src/Check_In_Paal.java
@@ -1,4 +1,21 @@
 public class Check_In_Paal {
     private double instapTarief;
 
+    public Check_In_Paal(double instapTarief) {
+        this.instapTarief = instapTarief;
+    }
+
+    public void inchecken(OV_Chipkaart kaart) {
+        if (!kaart.getGeldig()) {
+            System.out.println("Je kaart is niet geldig.");
+            return;
+        }
+        if (kaart.getSaldo() < instapTarief) {
+            System.out.println("Je hebt niet genoeg saldo.");
+            return;
+        }
+
+        kaart.setIsIngecheckt(true);
+        System.out.println("Je bent ingecheckt.");
+    }
 }
