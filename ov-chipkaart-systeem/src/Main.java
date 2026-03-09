a79f96f2dbd8b8ded9757aa8a7670bcd4959cd8f
Yonaki51 <yonivandon@hotmail.com>
2026-03-09 14:28:56 +0100

    main aangemaakt

--- ov-chipkaart-systeem/src/Main.java
+++ ov-chipkaart-systeem/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-OV_Chipkaart kaart = new OV_Chipkaart(10.00, "NL-1234", true);
-Check_In_Paal paal = new Check_In_Paal(1.50);
-paal.scanKaart(kaart);
+    public static void main(String[] args) {
+        OV_Chipkaart kaart = new OV_Chipkaart(10.00, "NL-1234", true);
+        Check_In_Paal paal = new Check_In_Paal(1.50);
+        paal.inchecken(kaart);
+    }
 }
