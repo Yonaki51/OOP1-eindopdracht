b4e2eb1d7a8d295d551a137b64a2bab23b501c57
Yonaki51 <yonivandon@hotmail.com>
2026-03-16 13:19:41 +0100

    chipnummer uit de creation van de kaart gehaald omdat dit niet meer nodig is

--- ov-chipkaart-systeem/src/Main.java
+++ ov-chipkaart-systeem/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        OV_Chipkaart kaart = new OV_Chipkaart(10.00, "NL-1234", true);
+        OV_Chipkaart kaart = new OV_Chipkaart(10.00, true);
         Check_In_Paal paal = new Check_In_Paal(1.50);
         paal.inchecken(kaart);
     }
