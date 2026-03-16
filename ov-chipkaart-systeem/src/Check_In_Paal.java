1d784fbe12dc27fa0031e0288ed8764807bbf333
Yonaki51 <yonivandon@hotmail.com>
2026-03-16 14:24:43 +0100

    onnodige returns weggehaald

--- ov-chipkaart-systeem/src/Check_In_Paal.java
+++ ov-chipkaart-systeem/src/Check_In_Paal.java
@@ -8,16 +8,17 @@ public class Check_In_Paal {
     public void inchecken(OV_Chipkaart kaart) {
         if (!kaart.getGeldig()) {
             System.out.println("Je kaart is niet geldig.");
-            return;
-        }
-        if (kaart.getSaldo() < instapTarief) {
+
+        } else if (kaart.getSaldo() < instapTarief) {
+
             System.out.println("Je hebt niet genoeg saldo.");
-            return;
+        } else {
+
+            kaart.setIsIngecheckt(true);
+            kaart.setSaldo(kaart.getSaldo() - instapTarief);
+            System.out.println("Je bent ingecheckt.");
         }
 
-        kaart.setIsIngecheckt(true);
-        kaart.setSaldo(kaart.getSaldo() -instapTarief);
-        System.out.println("Je bent ingecheckt.");
     }
 
     public void uitchecken(OV_Chipkaart kaart) {
@@ -26,12 +27,14 @@ public class Check_In_Paal {
 
         if (!kaart.getIsIngecheckt()) {
             System.out.println("Je bent niet ingecheckt.");
-            return;
+        }
+        else{
+
+            kaart.setIsIngecheckt(false);
+            kaart.setSaldo(kaart.getSaldo() + instapTarief - uitcheckbedrag);
+            System.out.println("je bent nu uitgecheckt.");
         }
 
-        kaart.setIsIngecheckt(false);
-        kaart.setSaldo(kaart.getSaldo() + instapTarief - uitcheckbedrag);
-        System.out.println("je bent nu uitgecheckt.");
     }
 
     public void toonSaldo(OV_Chipkaart kaart) {
