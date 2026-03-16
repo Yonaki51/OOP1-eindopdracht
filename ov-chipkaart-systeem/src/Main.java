4a95825d4a2154fd9e33058190b74b0c22666bee
Yonaki51 <yonivandon@hotmail.com>
2026-03-16 14:01:10 +0100

    nieuwe functies uitgeprobeerd in de main

--- ov-chipkaart-systeem/src/Main.java
+++ ov-chipkaart-systeem/src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         OV_Chipkaart kaart = new OV_Chipkaart(10.00, true);
         Check_In_Paal paal = new Check_In_Paal(1.50);
+        Oplaadpaal automaat = new Oplaadpaal();
         paal.inchecken(kaart);
+        paal.uitchecken(kaart);
+        paal.toonSaldo(kaart);
+        automaat.saldoOpwaarderen(kaart, 20);
+        automaat.toonSaldo(kaart);
     }
 }
