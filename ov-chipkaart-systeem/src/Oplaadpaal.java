4eb3dbd4e5c87197b30d435526f86326568645e1
Yonaki51 <yonivandon@hotmail.com>
2026-03-16 13:58:09 +0100

    oplaadpaal gemaakt. methode gegeven voor opwaarderen en het tonen van het saldo

--- ov-chipkaart-systeem/src/Oplaadpaal.java
+++ ov-chipkaart-systeem/src/Oplaadpaal.java
@@ -1,3 +1,14 @@
 public class Oplaadpaal {
 
+public void saldoOpwaarderen(OV_Chipkaart kaart, double bedrag){
+
+    kaart.setSaldo(kaart.getSaldo() + bedrag);
+    System.out.println("Je hebt " + bedrag + " op je kaart gezet.");
+
+}
+
+public void toonSaldo(OV_Chipkaart kaart) {
+        System.out.println("je nieuwe saldo is: " + kaart.getSaldo());
+    }
+
 }
